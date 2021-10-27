00d3ace506ee5adb03d2f629eecb4052bc04d842
wannarees <71537073+wannarees@users.noreply.github.com>
2021-10-27 15:24:11 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BiometricAuthen.Services;
+using BiometricAuthen.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("fabrands.otf", Alias = "fabrands")]
+[assembly: ExportFont("faregular.otf", Alias = "faregular")]
+[assembly: ExportFont("fasolid.otf", Alias = "fasolid")]
+
+namespace BiometricAuthen
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using BiometricAuthen.ViewModels;
+using BiometricAuthen.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BiometricAuthen
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
